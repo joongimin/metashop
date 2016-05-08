93254c0f6ea6cf3854ad35bf919e00fce4330d35
Joon Min <jm@cre.ma>
2016-05-08 16:17:28 +0900

    EDIT - prep for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+set :application, 'metashop-rails'
+set :user, 'ubuntu'
+set :github, 'joongimin/metashop-rails'
+set :git_strategy, CremaGitStrategy
+set :repo_tree, fetch(:application)
+set :repo_subtree, 'core'
+set :repo_url, "git@github.com:#{fetch(:github)}.git"
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :ssh_options, forward_agent: true
+set :rbenv_ruby, '2.3.1'
+set :templates_path, 'lib/capistrano/tasks/templates'
+
+def template_file(name)
+  "#{fetch(:templates_path)}/#{name}"
+end
+
+def template(name, target)
+  config_file = template_file(name)
+  upload! StringIO.new(ERB.new(File.read(config_file)).result(binding)), target
+end
+
+after 'deploy:publishing', 'deploy:restart'
+
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+role :app, 'hae1.hana-choi.com'
+role :web, 'hae1.hana-choi.com'
+role :db, 'hae1.hana-choi.com'
+set :rails_env, 'production'
+set :host, 'metashop.hana-choi.com'
+set :unicorn_workers, 2
+set :branch, 'master'
